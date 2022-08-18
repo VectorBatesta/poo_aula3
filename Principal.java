26022aaaf4eb07e6bc8e2cb4527c33b9126ea614
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2022-08-18 17:19:36 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -7,14 +7,18 @@ package aula3;
 public class Principal {
 
     public Principal(){
-        Estudante joao = new Estudante("joao");
+        Estudante joao = new Estudante("joao", "rua 12344", "123123", 10);
+        Estudante marcos = new Estudante("marcos", "rua 1241", "616122", 5);
+        Estudante manoLoco = new Estudante("MAAANO", "rua nenhum", "000000", 99999);
         //joao.setNome("joao");
-        System.out.println(joao.getNome());
+        //System.out.println(joao.getNome());
+        
+        System.out.println(joao.toString());
+        System.out.println(marcos.toString());
+        System.out.println(manoLoco.toString());
     }
     
     public static void main(String[] args) {
         Principal objeto = new Principal();
-        
     }
-    
 }
